588f256ed59c33b0564977c69e930b73f003ab9a
HrishikeshThakkar-github <hrishi.pvt@gmail.com>
2025-03-27 20:12:24 +0530

    Freshcery: frontend-template developed and added a databse configrationto the configration folder

---
+++ configration/db.config.php
@@ -0,0 +1,29 @@
+<?php
+
+
+//now to restrict the viewership of this page directly through the url we ca nuse:
+
+if(!isset($_SERVER['HTTP_REFERER'])){
+    //when someone try to access by url gets redirected to homepage-> index.php
+    header("Location: http/index.php");
+}
+
+$dsn = "mysql:host=localhost;dbname=freshcery;charset=utf8mb4";
+$db_user = "root";  
+$db_pass = "Simform@123";  
+
+try {
+    $pdo = new PDO($dsn, $db_user, $db_pass);
+    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    echo "connected";
+} catch (PDOException $e) {
+    echo $e->getMessage();
+}
+
+
+//display startup error
+//error reporting
+//display errpr to printout errors
+
+//ini_set('display_startup_errors', 1);ini_set('display_errors', 1);error_reporting(-1);
+?> 
